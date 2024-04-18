f87614634d1549baebe2543ed3e5a27cce3f6b65
techcaptain04 <andrii.seliuk@outlook.com>
2024-04-18 09:56:38 +0800

    ADD Github Source

---
+++ src/components/github/Github.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+    import Icon from './Icon.svelte';
+</script>
+
+
+<style scoped>
+    div {
+        height: 25px;
+        position: absolute;
+        bottom: 0;
+        margin: 5px;
+        padding: 5px 5px;
+        background-color: #161614;
+        border-radius: 5px;
+    }
+</style>
+
+<a href="https://github.com/jjspscl/i-calculate" target="_blank">
+    <div>
+        <Icon/>
+    </div>
+</a>
